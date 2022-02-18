f291339e76a09df603f30f76a424b4353985a1b1
jpvieirah <jp201101@gmail.com>
2022-02-18 09:34:05 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
 
 consign().include('routes').include('utils').into(app);
 
-
-app.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', ()=>{
 
     console.log('servidor rodando!');
-//a variavel que criamos do server vai ficar ouvindo na porta 3000, ip e função de callback//
+
 });
